865385261b9dad225bf0ffaa00585d4f8b4ae1b1
rilence <nemanja.ristic@live.com>
2018-08-02 08:41:08 +0200

    Payment page layout, styling changes

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -7,10 +7,30 @@ import {DataService} from '../common';
 })
 export class PaymentComponent implements OnInit {
 
+  paymentWallet: any = [];
+  paymentWalletHistory: any = [];
+  activePaymentWallet: any = [];
+
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.getPaymentWallet();
+  }
 
+  getPaymentWallet() {
+    this.dataService.getPaymentWallet().subscribe((data) => {
+        this.paymentWallet = data;
+        console.log(data);
+        // this.getPaymentWalletHistory(this.paymentWallet[0].token_address);
+    });
+  }
+
+  getPaymentWalletHistory(index) {
+    this.activePaymentWallet = this.paymentWallet[index].token_address;
+    this.dataService.getPaymentWalletHistory(this.activePaymentWallet).subscribe((data) => {
+        this.paymentWalletHistory = data;
+        console.log(data);
+    });
   }
 
 }
